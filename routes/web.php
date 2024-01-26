c86f5390f90b3cb8c6925cd11d09c151334c339d
Maxenor <maxime.michon@outlook.com>
2024-01-26 10:52:19 +0100

    feat : integrated design named routes

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts', [ 'posts' => Post::latest()->get()]);
-});
+    return view('posts', ['posts' => Post::latest()->get(), 'categories' => Category::all()]);
+})->name('home');
 
 // find the post where the post's id is the id passed and load the view
 Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [ 'post' => $post]);
+    return view('post', ['post' => $post]);
 });
 // find the posts with the categories corresponding to the slug, load is eager loading on existing model
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [ 'posts' => $category->posts]);
-});
+Route::get('categories/{category:name}', function (Category $category) {
+    return view('posts',
+        ['posts' => $category->posts, 'categories' => Category::all(), 'currentCategory' => $category]);
+})->name('category');
+
 Route::get('author/{author:username}', function (User $author) {
-    return view('posts', [ 'posts' => $author->posts]);
+    return view('posts', ['posts' => $author->posts, 'categories' => Category::all()]);
 });
